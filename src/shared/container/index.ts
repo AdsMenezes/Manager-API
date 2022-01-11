ba64a51e48a3390b585c823eab5f65dc1955294c
Anderson Menezes <anderson@codrop.com.br>
2022-01-11 17:03:09 -0300

    :sparkles: Adding resource HashProvider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe'
 
+import './providers'
+
 import IUsersRepository from '@modules/users/domain/repositories/IUsersRepository'
 import UsersRepository from '@modules/users/infrastructure/typeorm/repositories/UsersRepository'
 
