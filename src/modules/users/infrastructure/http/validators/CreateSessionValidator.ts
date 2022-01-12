134b8f5ac76670050e86c3dfc5ad5622bed95085
Anderson Menezes <anderson@codrop.com.br>
2022-01-12 20:22:14 -0300

    :lock: Converting email input to lowercase

--- src/modules/users/infrastructure/http/validators/CreateSessionValidator.ts
+++ src/modules/users/infrastructure/http/validators/CreateSessionValidator.ts
@@ -2,7 +2,7 @@ import { celebrate, Segments, Joi } from 'celebrate'
 
 export default celebrate({
   [Segments.BODY]: {
-    email: Joi.string().email().max(40).required(),
+    email: Joi.string().email().max(40).required().lowercase(),
     password: Joi.string().min(3).max(20).required(),
   },
 })
--- src/modules/users/infrastructure/http/validators/CreateUserValidator.ts
+++ src/modules/users/infrastructure/http/validators/CreateUserValidator.ts
@@ -3,7 +3,7 @@ import { celebrate, Segments, Joi } from 'celebrate'
 export default celebrate({
   [Segments.BODY]: {
     name: Joi.string().min(3).max(20).required(),
-    email: Joi.string().email().max(40).required(),
+    email: Joi.string().email().max(40).required().lowercase(),
     password: Joi.string().min(3).max(20).required(),
     phone: Joi.string()
       .regex(/^\([0-9]{2}\)\s[0-9]{5}-[0-9]{4}$/)
